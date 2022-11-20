23c46889ac92d89bfe72a760808f33be8e270aed
Lasse <lass5588@icloud.com>
2022-11-20 20:38:18 +0100

    Added logic for kepping the CLI running and taking the basic input start, pause and info of current track, which lacks an implementation.

--- main.py
+++ main.py
@@ -10,9 +10,43 @@ SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 SPOTIPY_REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
 
-scope = 'user-modify-playback-state'
+scope = "user-modify-playback-state, user-library-read, user-read-playback-state"
 
 token = SpotifyOAuth(scope = scope, client_id = SPOTIPY_CLIENT_ID, client_secret = SPOTIPY_CLIENT_SECRET, redirect_uri = SPOTIPY_REDIRECT_URI)
-spotifyObject = spotipy.Spotify(auth_manager = token)
+spotify = spotipy.Spotify(auth_manager = token)
 
-spotifyObject.pause_playback()
+def parser():
+    print("enter command: ", end="")
+    raw_input = input()
+
+    match raw_input:
+        case "pause" | "Pause" | "PAUSE":
+            stop_playback()
+        case "play" | "Play" | "PLAY":
+            start_playback()
+        case "current" | "Current" | "CURRENT":
+            get_current_track()
+        case _:
+            print(f"Command not exepted: {raw_input}")
+
+def start_playback():
+    try:
+        spotify.start_playback()
+    except:
+        print("Illegal command play, already playing, else check connection.")
+
+def stop_playback():
+    try:
+        spotify.pause_playback()
+    except:
+        print("Illegal command, already paused, else check connection ")
+
+def get_current_track():
+    try:
+        result = spotify.current_playback()
+        print(result['device']['is_active'], " – ", result['progress_ms']/1000,)
+    except:
+        print("", end="")
+
+while(True):
+    parser()
